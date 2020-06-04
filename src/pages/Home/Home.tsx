f970e5b1506c772fe2b893f1ea2b6cf9132c30f8
Bruno Lima <brunodplima@gmail.com>
2020-06-04 18:27:56 -0300

    Configurar navegação
    Configurar navegação entre telas da aplicação utilizando
    StackNavigator.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,8 +2,15 @@ import React from 'react'
 import { Feather as FeatherIcon } from '@expo/vector-icons'
 import { View, Text, Image, StyleSheet, ImageBackground } from 'react-native'
 import { RectButton } from 'react-native-gesture-handler'
+import { useNavigation } from '@react-navigation/native'
 
 const Home = () => {
+  const navigation = useNavigation()
+
+  const handleNavigateToPoints = () => {
+    navigation.navigate('Points')
+  }
+
   return (
     <ImageBackground
       source={require('../../assets/home-background.png')}
@@ -17,7 +24,7 @@ const Home = () => {
       </View>
 
       <View style={styles.footer}>
-        <RectButton style={styles.button} onPress={() => {}}>
+        <RectButton style={styles.button} onPress={handleNavigateToPoints}>
           <View style={styles.buttonIcon}>
             <FeatherIcon name="arrow-right" color="#FFFFFF" size={24}/>
           </View>
@@ -34,7 +41,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 32,
-    backgroundColor: '#f0f0f5'
   },
 
   main: {
