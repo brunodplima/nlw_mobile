1a7e69e11afd9b9289920a3b0111fbc6993cba26
Bruno Lima <brunodplima@gmail.com>
2020-06-05 18:45:15 -0300

    Criar página do mapa

---
+++ README.md
@@ -0,0 +1,22 @@
+# nlw_mobile
+Projeto criado durante a Next Level Week da Rocketseat (01 a 07 de junho de 2020).
+
+## Install
+
+Copie o arquivo `.env`, com o seguinte comando:
+
+    $ cp .env .env.local
+
+Em seguida, preencha as informações necessárias.
+
+Por fim, basta instalar as dependências com...
+
+    $ yarn install
+
+## Running the project
+
+    $ yarn start
+<!--
+## Simple build for production
+
+    $ yarn build -->
--- src/pages/Points/Points.tsx
+++ src/pages/Points/Points.tsx
@@ -1,10 +1,168 @@
 import React from 'react'
-import { View } from 'react-native'
+import { Feather as FeatherIcon } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
+import { SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native'
+import MapView, { Marker } from 'react-native-maps'
+import { SvgUri } from 'react-native-svg'
 
 const Points = () => {
+  const navigation = useNavigation()
+
+  const handleNavigateBack = () => {
+    navigation.goBack()
+  }
+
+  const handleNavigateToDetail = () => {
+    navigation.navigate('Detail')
+  }
+
   return (
-    <View />
+    <SafeAreaView style={{flex: 1}}>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handleNavigateBack}>
+          <FeatherIcon name="arrow-left" size={20} color="#34cb79" />
+        </TouchableOpacity>
+
+        <Text style={styles.title}>Bem-vindo.</Text>
+        <Text style={styles.description}>Encontre no mapa um ponto de coleta.</Text>
+
+        <View style={styles.mapContainer}>
+          <MapView
+            style={styles.map}
+            initialRegion={{
+              latitude: -27.2092052,
+              longitude: -49.6401092,
+              latitudeDelta: 0.014,
+              longitudeDelta: 0.014,
+            }}
+          >
+            <Marker
+              style={styles.mapMarker}
+              coordinate={{
+                latitude: -27.2092052,
+                longitude: -49.6401092,
+              }}
+              onPress={handleNavigateToDetail}
+            >
+              <View style={styles.mapMarkerContainer}>
+                <Image style={styles.mapMarkerImage} source={{ uri: 'https://via.placeholder.com/150' }}/>
+                <Text style={styles.mapMarkerTitle}>Title</Text>
+              </View>
+            </Marker>
+          </MapView>
+        </View>
+      </View>
+      <View style={styles.itemsContainer}>
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{ paddingHorizontal: 20 }}
+        >
+          {[0,1,2,3,4,5].map((key) => (<TouchableOpacity key={key} style={styles.item} onPress={() => {}}>
+            <SvgUri width={42} height={42} uri="http://192.168.0.14:3333/uploads/oleo.svg" />
+            <Text style={styles.itemTitle}>Óleo</Text>
+          </TouchableOpacity>))}
+        </ScrollView>
+      </View>
+    </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 32,
+    paddingTop: 20,
+  },
+
+  title: {
+    fontSize: 20,
+    fontFamily: 'Ubuntu_700Bold',
+    marginTop: 24,
+  },
+
+  description: {
+    color: '#6C6C80',
+    fontSize: 16,
+    marginTop: 4,
+    fontFamily: 'Roboto_400Regular',
+  },
+
+  mapContainer: {
+    flex: 1,
+    width: '100%',
+    borderRadius: 10,
+    overflow: 'hidden',
+    marginTop: 16,
+  },
+
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+
+  mapMarker: {
+    width: 90,
+    height: 80,
+  },
+
+  mapMarkerContainer: {
+    width: 90,
+    height: 70,
+    backgroundColor: '#34CB79',
+    flexDirection: 'column',
+    borderRadius: 8,
+    overflow: 'hidden',
+    alignItems: 'center'
+  },
+
+  mapMarkerImage: {
+    width: 90,
+    height: 45,
+    resizeMode: 'cover',
+  },
+
+  mapMarkerTitle: {
+    flex: 1,
+    fontFamily: 'Roboto_400Regular',
+    color: '#FFF',
+    fontSize: 13,
+    lineHeight: 23,
+  },
+
+  itemsContainer: {
+    flexDirection: 'row',
+    marginTop: 16,
+    marginBottom: 32,
+  },
+
+  item: {
+    backgroundColor: '#fff',
+    borderWidth: 2,
+    borderColor: '#eee',
+    height: 120,
+    width: 120,
+    borderRadius: 8,
+    paddingHorizontal: 16,
+    paddingTop: 20,
+    paddingBottom: 16,
+    marginRight: 8,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+
+    textAlign: 'center',
+  },
+
+  selectedItem: {
+    borderColor: '#34CB79',
+    borderWidth: 2,
+  },
+
+  itemTitle: {
+    fontFamily: 'Roboto_400Regular',
+    textAlign: 'center',
+    fontSize: 13,
+  },
+});
 
 export default Points
